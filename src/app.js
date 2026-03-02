cfdc940c49b02bbb8b212edfb66510a3594348ad
Thiago Oliveira <thiago.math@hotmail.com>
2026-03-02 18:58:03 -0300

    cria metodos https

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const route = router.get("/", (req, res, next) => {
   res.status(200).send({
@@ -13,6 +16,25 @@ const route = router.get("/", (req, res, next) => {
   });
 });
 
+const create = router.post("/", (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(201).send({
+    id: id,
+    item: req.body,
+  });
+});
+
+const del = router.delete("/", (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use(router);
+app.use("/products", create);
+app.use("/products", put);
+app.use("/products", del);
 
 module.exports = app;
