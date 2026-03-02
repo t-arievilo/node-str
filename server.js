1ae43478d1f84967346558e4da79e6059f1f5221
Thiago Oliveira <thiago.math@hotmail.com>
2026-03-02 18:10:13 -0300

    cria normalizerPort, onError e onListening

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
 const http = require("http");
 const express = require("express");
 const { version } = require("os");
+const { parse } = require("path");
 const debug = require("debug")("nodestr:server");
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
@@ -22,4 +23,44 @@ const route = router.get("/", (req, res, next) => {
 app.use(router);
 
 server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
 console.log("servidor rodando na porta " + port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
