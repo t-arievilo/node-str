e556ea78d9f4480f6493d037f7d84e0b6f487588
Thiago Oliveira <thiago.math@hotmail.com>
2026-03-03 22:12:14 -0300

    cria a model Customer

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
 
 //Carregamento dos Models
 const Product = require("./models/product");
+const Customer = require("./models/customer");
 
 //Rotas
 const indexRoute = require("./routes/index-route");
